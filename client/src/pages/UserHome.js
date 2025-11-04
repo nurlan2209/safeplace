a6ced16e954ff014ca7c3d3f8da14233c60a0581
nurlan2209 <nkuanyshuly2020@gmail.com>
2025-11-04 08:58:48 +0500

    mob.adapt

--- client/src/pages/UserHome.js
+++ client/src/pages/UserHome.js
@@ -8,12 +8,10 @@ const UserHome = () => {
       <Header isLogged={true} />
       <main className="user-home">
         <section className="left">
-          <h1>SafePlace —<br /> your safety in your hands</h1>
+          <h1>
+            SafePlace —<br /> ваша безопасность в ваших руках
+          </h1>
           <p className="subtitle">Место, где тебя понимают</p>
-          <div className="buttons">
-            <Link to="/profile" className="btn">Профиль</Link>
-            <Link to="/" className="btn btn-light">Выйти</Link>
-          </div>
         </section>
 
         <section className="right">
@@ -21,22 +19,30 @@ const UserHome = () => {
             <div className="section-card">
               <h3>Форум</h3>
               <p>Истории, статьи, обсуждения.</p>
-              <Link to="/forum" className="tile-btn">Перейти</Link>
+              <Link to="/forum" className="tile-btn">
+                Перейти
+              </Link>
             </div>
             <div className="section-card">
               <h3>Мои сохранённые</h3>
               <p>Избранное и важные записи.</p>
-              <Link to="/favorites" className="tile-btn">Перейти</Link>
+              <Link to="/favorites" className="tile-btn">
+                Перейти
+              </Link>
             </div>
             <div className="section-card">
               <h3>Сообщения</h3>
               <p>Анонимное общение с людьми.</p>
-              <Link to="/messages" className="tile-btn">Перейти</Link>
+              <Link to="/messages" className="tile-btn">
+                Перейти
+              </Link>
             </div>
             <div className="section-card">
               <h3>Помощь</h3>
               <p>Психологи, горячие линии, международные ресурсы.</p>
-              <Link to="/help" className="tile-btn">Перейти</Link>
+              <Link to="/help" className="tile-btn">
+                Перейти
+              </Link>
             </div>
           </div>
         </section>
